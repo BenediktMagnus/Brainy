75699776d7f2229fce5b0ed65d994fb2d3169271
Benedikt Magnus <magnus@magnuscraft.de>
2023-10-29 01:17:20 +0200

    Fixed duplicate syscode constant compilation.

--- runtime/src/linuxAmd64/syscodes.h
+++ runtime/src/linuxAmd64/syscodes.h
@@ -2,11 +2,11 @@
 
 #include "../common/types.h"
 
-const UInt8 SyscodeRead = 0x00;
-const UInt8 SyscodeWrite = 0x01;
+static const UInt8 SyscodeRead = 0x00;
+static const UInt8 SyscodeWrite = 0x01;
 
-const UInt8 SyscodeMemoryMap = 0x09;
+static const UInt8 SyscodeMemoryMap = 0x09;
 
-const UInt8 SyscodeMemoryUnmap = 0x0b;
+static const UInt8 SyscodeMemoryUnmap = 0x0b;
 
-const UInt8 SyscodeExit = 0x3c;
+static const UInt8 SyscodeExit = 0x3c;
